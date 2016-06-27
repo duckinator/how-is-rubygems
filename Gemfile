00724f49d5c89c130973b3066f9766c50a50d18e
Ellen Marie Dash <me@duckie.co>
2016-06-27 16:37:32 -0400

    Implement generate-report.rb.

---
+++ Gemfile
@@ -0,0 +1,1 @@
+gem 'how_is'
---
+++ generate-report.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'how_is'
+
+date = Date.strptime(Time.now.to_i.to_s, '%s').strftime('%Y-%m-%d')
+filename = "_posts/#{date}-report.html"
+
+header = <<EOF
+---
+title: #{date} Report
+layout: default
+---
+EOF
+
+report = HowIs.generate_report(repository: 'rubygems/rubygems')
+
+File.open(filename, 'w') do |f|
+  f.puts header
+  f.puts
+  f.puts report
+end
