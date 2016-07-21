f410e74c001c533261ca8292d390484574491fcb
Ellen Marie Dash <me@duckie.co>
2016-07-21 08:57:21 -0400

    Remove dead code from generate-report.rb.

--- generate-report.rb
+++ generate-report.rb
@@ -43,32 +43,3 @@ config['reports'].each do |format, report_config|
     f.puts report
   end
 end
-
-__END__
-
-# JSON Report.
-
-json_filename = "json/#{date}.json"
-
-json_report = HowIs.generate_report()
-
-File.open(json_filename, 'w') {|f| f.puts json_report }
-
-# HTML Report.
-
-html_filename = "_posts/#{date}-report.html"
-
-html_header = <<EOF
----
-title: #{date} Report
-layout: default
----
-EOF
-
-html_report = HowIs.generate_report(repository: 'rubygems/rubygems', from_file: json_filename, format: 'html')
-
-File.open(html_filename, 'w') do |f|
-  f.puts html_header
-  f.puts
-  f.puts html_report
-end
