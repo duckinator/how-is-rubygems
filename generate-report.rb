fdc5ba29cb35753c2a82a358c67e43ddbce9acc5
Ellen Marie Dash <me@duckie.co>
2016-07-21 08:56:44 -0400

    Rewrite generate-report.rb in preparation for splitting out how_is_web.

--- generate-report.rb
+++ generate-report.rb
@@ -1,14 +1,56 @@
 #!/usr/bin/env ruby
 
 require 'how_is'
+require 'yaml'
 
-date = Date.strptime(Time.now.to_i.to_s, '%s').strftime('%Y-%m-%d')
+config = YAML.load_file('how_is.yml')
+
+date = Date.strptime(Time.now.to_i.to_s, '%s')
+date_string = date.strftime('%Y-%m-%d')
+friendly_date = date.strftime('%B %d, %y')
+
+analysis = HowIs.generate_analysis(repository: config['repository'])
+
+report_data = {
+  repository: config['repository'],
+  date: date,
+  friendly_date: friendly_date,
+}
+
+def jekyll_header(jekyll_config, report_data)
+  headers = jekyll_config.map { |k, v|
+    v = v % report_data
+
+    [k, v]
+  }.to_h
+
+  YAML.dump(headers)
+end
+
+config['reports'].each do |format, report_config|
+  filename = report_config['filename'] % report_data
+  file = File.join(report_config['directory'], filename)
+
+  report = HowIs::Report.export(analysis, format)
+
+  File.open(file, 'w') do |f|
+    if report_config['jekyll']
+      f.puts jekyll_header(report_config['jekyll'], report_data)
+      f.puts "---"
+      f.puts
+    end
+
+    f.puts report
+  end
+end
+
+__END__
 
 # JSON Report.
 
 json_filename = "json/#{date}.json"
 
-json_report = HowIs.generate_report(repository: 'rubygems/rubygems', format: 'json')
+json_report = HowIs.generate_report()
 
 File.open(json_filename, 'w') {|f| f.puts json_report }
 
